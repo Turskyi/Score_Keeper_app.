ceb87a52825e08e66dbaf5ad228514af219c8b2e
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2019-03-16 16:34:15 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,93 +1,332 @@
 package com.example.android.courtcounter;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int scoreTeamA = 0;
-    int scoreTeamB = 0;
-    int foulsTeamA = 0;
-    int foulsTeamB = 0;
+    int timeDelay;
+    int timeDelayRun;
+    int minTimeDelay;
+    int timeDecrease;
+    int scoreTeamA;
+    int scoreTeamB;
+    int foulsTeamA;
+    int foulsTeamB;
+    Message message;
+    Bundle bundle;
+    Handler handlerScoreA, handlerFoulsA, handlerScoreB, handlerFoulsB;
+    Button btnReset, btnPlusA, btnPlusB, btnMinusA, btnMinusB,
+            btnMinusFoulsA, btnMinusFoulB, btnPlusFoulA, btnPlusFoulB;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //          Decrease the score for Team A by 1 point.
-        Button btnMinusA = findViewById(R.id.minus_one_a);
-        btnMinusA.setOnClickListener(new View.OnClickListener() {
+        message = new Message();
+        bundle = new Bundle();
+        btnReset = findViewById(R.id.reset);
+        btnPlusA = findViewById(R.id.plus_one_a);
+        btnPlusB = findViewById(R.id.plus_one_b);
+        btnMinusA = findViewById(R.id.minus_one_a);
+        btnMinusB = findViewById(R.id.minus_one_b);
+        btnMinusFoulsA = findViewById(R.id.minus_foul_a);
+        btnMinusFoulB = findViewById(R.id.minus_foul_b);
+        btnPlusFoulA = findViewById(R.id.plus_foul_a);
+        btnPlusFoulB = findViewById(R.id.plus_foul_b);
+
+        handlerFoulsA = new Handler(Looper.getMainLooper()) {
             @Override
-            public void onClick(View v) {
-                if (scoreTeamA == 0) return;
-                scoreTeamA -= 1;
-                displayForTeamA(scoreTeamA);
+            public void handleMessage(final Message msg) {
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        displayFoulsForTeamA(msg.getData().getInt("KEY"));
+                    }
+                });
+                super.handleMessage(msg);
+            }
+        };
+
+        handlerFoulsB = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(final Message msg) {
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        displayFoulsForTeamB(msg.getData().getInt("KEY"));
+                    }
+                });
+                super.handleMessage(msg);
+            }
+        };
+
+        handlerScoreA = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(final Message msg) {
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        displayForTeamA(msg.getData().getInt("KEY"));
+                    }
+                });
+                super.handleMessage(msg);
+            }
+        };
+
+        handlerScoreB = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(final Message msg) {
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        displayForTeamB(msg.getData().getInt("KEY"));
+                    }
+                });
+                super.handleMessage(msg);
+            }
+        };
+
+        //          Decrease the score for Team A by 1 point.
+        btnMinusA.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, final MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            timeDelay = 1000;
+                            timeDelayRun = 100;
+                            minTimeDelay = 10;
+                            timeDecrease = 2;
+                            do {
+                                if (scoreTeamA == 0) return;
+                                scoreTeamA--;
+                                bundle.putInt("KEY", scoreTeamA);
+                                handlerScoreA.handleMessage(message);
+                                speedUp();
+                            } while (event.getAction() != MotionEvent.ACTION_UP);
+                        }
+                    }).start();
+                    return true;
+                } else return event.getAction() == MotionEvent.ACTION_UP;
             }
         });
 
         //          Decrease the score for Team B by 1 point.
-        Button btnMinusB = findViewById(R.id.minus_one_b);
-        btnMinusB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (scoreTeamB == 0) return;
-                scoreTeamB -= 1;
-                displayForTeamB(scoreTeamB);
+        btnMinusB.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, final MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            timeDelay = 1000;
+                            timeDelayRun = 100;
+                            minTimeDelay = 10;
+                            timeDecrease = 2;
+                            do {
+                                if (scoreTeamB == 0) return;
+                                scoreTeamB--;
+                                bundle.putInt("KEY", scoreTeamB);
+                                handlerScoreB.handleMessage(message);
+                                speedUp();
+                            } while (event.getAction() != MotionEvent.ACTION_UP);
+                        }
+                    }).start();
+                    return true;
+                } else return event.getAction() == MotionEvent.ACTION_UP;
             }
         });
 
         //          Decrease fouls for Team A by 1.
-        Button btnMinusFoulA = findViewById(R.id.minus_foul_a);
-        btnMinusFoulA.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (foulsTeamA == 0) return;
-                foulsTeamA -= 1;
-                displayFoulForTeamA(foulsTeamA);
+        btnMinusFoulsA.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, final MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            timeDelay = 1000;
+                            timeDelayRun = 100;
+                            minTimeDelay = 10;
+                            timeDecrease = 2;
+                            do {
+                                if (foulsTeamA == 0) return;
+                                foulsTeamA--;
+                                bundle.putInt("KEY", foulsTeamA);
+                                handlerFoulsA.handleMessage(message);
+                                speedUp();
+                            } while (event.getAction() != MotionEvent.ACTION_UP);
+                        }
+                    }).start();
+                    return true;
+                } else return event.getAction() == MotionEvent.ACTION_UP;
             }
         });
-
+        
         //          Decrease fouls for Team B by 1.
-        Button btnMinusFoulB = findViewById(R.id.minus_foul_b);
-        btnMinusFoulB.setOnClickListener(new View.OnClickListener() {
+        btnMinusFoulB.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, final MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            timeDelay = 1000;
+                            timeDelayRun = 100;
+                            minTimeDelay = 10;
+                            timeDecrease = 2;
+                            do {
+                                if (foulsTeamB == 0) return;
+                                foulsTeamB--;
+                                bundle.putInt("KEY", foulsTeamB);
+                                handlerFoulsB.handleMessage(message);
+                                speedUp();
+                            } while (event.getAction() != MotionEvent.ACTION_UP);
+                        }
+                    }).start();
+                    return true;
+                } else return event.getAction() == MotionEvent.ACTION_UP;
+            }
+        });
+
+//        Increase foul for Team A by 1 very fast.
+        btnPlusFoulA.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, final MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            timeDelay = 1000;
+                            timeDelayRun = 100;
+                            minTimeDelay = 10;
+                            timeDecrease = 2;
+                            do {
+                                foulsTeamA++;
+                                bundle.putInt("KEY", foulsTeamA);
+                                handlerFoulsA.handleMessage(message);
+                                speedUp();
+                            } while (event.getAction() != MotionEvent.ACTION_UP);
+                        }
+                    }).start();
+                    return true;
+                } else return event.getAction() == MotionEvent.ACTION_UP;
+            }
+        });
+
+        /*
+          Increase the score for Team A by 1 point.
+         */
+        btnPlusA.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, final MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            timeDelay = 1000;
+                            timeDelayRun = 100;
+                            minTimeDelay = 10;
+                            timeDecrease = 2;
+                            do {
+                                scoreTeamA++;
+                                bundle.putInt("KEY", scoreTeamA);
+                                handlerScoreA.handleMessage(message);
+                                speedUp();
+                            } while (event.getAction() != MotionEvent.ACTION_UP);
+                        }
+                    }).start();
+                    return true;
+                } else return event.getAction() == MotionEvent.ACTION_UP;
+            }
+        });
+
+        /*
+          Increase the score for Team A by 1 point.
+         */
+        btnPlusB.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, final MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            timeDelay = 1000;
+                            timeDelayRun = 100;
+                            minTimeDelay = 10;
+                            timeDecrease = 2;
+                            do {
+                                scoreTeamB++;
+                                bundle.putInt("KEY", scoreTeamB);
+                                handlerScoreB.handleMessage(message);
+                                speedUp();
+                            } while (event.getAction() != MotionEvent.ACTION_UP);
+                        }
+                    }).start();
+                    return true;
+                } else return event.getAction() == MotionEvent.ACTION_UP;
+            }
+        });
+
+        /*
+          Increase fouls for Team B by 1.
+         */
+        btnPlusFoulB.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, final MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            timeDelay = 1000;
+                            timeDelayRun = 100;
+                            minTimeDelay = 10;
+                            timeDecrease = 2;
+                            do {
+                                foulsTeamB++;
+                                bundle.putInt("KEY", foulsTeamB);
+                                handlerFoulsB.handleMessage(message);
+                                speedUp();
+                            } while (event.getAction() != MotionEvent.ACTION_UP);
+                        }
+                    }).start();
+                    return true;
+                } else return event.getAction() == MotionEvent.ACTION_UP;
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (foulsTeamB == 0) return;
-                foulsTeamB -= 1;
-                displayFoulForTeamB(foulsTeamB);
+                scoreTeamA = 0;
+                scoreTeamB = 0;
+                foulsTeamA = 0;
+                foulsTeamB = 0;
+                displayForTeamA(scoreTeamA);
+                displayForTeamB(scoreTeamB);
+                displayFoulsForTeamA(foulsTeamA);
+                displayFoulsForTeamB(foulsTeamB);
             }
         });
     }
 
-    /**
-     * Increase the score for Team A by 1 point.
-     */
-    public void addOneForTeamA(View v) {
-        scoreTeamA += 1;
-        displayForTeamA(scoreTeamA);
-    }
-
-    /**
-     * Increase foul for Team A by 1.
-     */
-    public void addFoulForTeamA(View v) {
-        foulsTeamA += 1;
-        displayFoulForTeamA(foulsTeamA);
-    }
-
-    public void resetScore(View v) {
-        scoreTeamA = 0;
-        scoreTeamB = 0;
-        foulsTeamA = 0;
-        foulsTeamB = 0;
-        displayForTeamA(scoreTeamA);
-        displayForTeamB(scoreTeamB);
-        displayFoulForTeamA(foulsTeamA);
-        displayFoulForTeamB(foulsTeamB);
+    private void speedUp() {
+        message.setData(bundle);
+        try {
+            Thread.sleep(timeDelay);
+            if (timeDelay > minTimeDelay) {
+                timeDelay -= timeDecrease;
+            }
+            if (timeDelay > timeDelayRun) {
+                timeDelay = timeDelayRun;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -109,7 +348,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Displays the given fouls for Team A.
      */
-    public void displayFoulForTeamA(int foul) {
+    public void displayFoulsForTeamA(int foul) {
         TextView foulsView = findViewById(R.id.team_a_fouls);
         foulsView.setText(String.valueOf(foul));
     }
@@ -117,24 +356,8 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Displays the given fouls for Team B.
      */
-    public void displayFoulForTeamB(int fouls) {
+    public void displayFoulsForTeamB(int fouls) {
         TextView foulsView = findViewById(R.id.team_b_fouls);
         foulsView.setText(String.valueOf(fouls));
-    }
-
-    /**
-     * Increase the score for Team A by 1 point.
-     */
-    public void addOneForTeamB(View v) {
-        scoreTeamB += 1;
-        displayForTeamB(scoreTeamB);
-    }
-
-    /**
-     * Increase fouls for Team B by 1.
-     */
-    public void addFoulForTeamB(View v) {
-        foulsTeamB += 1;
-        displayFoulForTeamB(foulsTeamB);
     }
 }
